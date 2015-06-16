67ab4a000231ab3c77fdd6a2fcb553cfb51bf6f8
summery <hellosummer0604@gmail.com>
2015-06-16 17:51:52 -0400

    project mapping file

--- src/com/teamapp/controller/UserController.java
+++ src/com/teamapp/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,6 +19,7 @@ import com.teamapp.service.user.IUserService;
 
 @Controller
 @RequestMapping(value="/user")
+@Scope("prototype")
 public class UserController {
 	
 	@Resource
